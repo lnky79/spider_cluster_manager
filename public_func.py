2c8bfd894ceb64e210172adeee7efa52f453c766
lyn716 <965606089@qq.com>
2017-02-15 16:35:09 +0800

    修改datatime序列化的bug

--- public_func.py
+++ public_func.py
@@ -30,13 +30,16 @@ class Timer:
 
 class CJsonEncoder(json.JSONEncoder):
     def default(self,obj):
-        if isinstance(obj,datetime.datetime):
+        if isinstance(obj,datetime):
             return obj.strftime( '%Y-%m-%d %H:%M:%S')
         elif isinstance(obj, datetime.date):
             return obj.strftime( "%Y-%m-%d")
         else:
             return json.JSONEncoder.default(self,obj)
 
+
+
+from bson import json_util
 
 def json_response(func):
     """
--- tool/rq_func.py
+++ tool/rq_func.py
@@ -11,20 +11,29 @@
 """
 from db_config import Session
 from .models import ProxyServerORM
+import time
 
-def free_back_proxy_item(proxy_server=None,ip=None):
+def free_back_proxy_item(ip,wait_seconds=0):
     db_session = Session()
-    if ip and proxy_server is None:
+    try:
         proxy_server = db_session\
-            .query(ProxyServerORM)\
-            .filter_by(ip=ip)[0]
+            .query(ProxyServerORM).filter_by(ip=ip)[0]
+    except IndexError:
+        err = 'No such ip in database : {}'.format(ip)
+        print(err)
+        raise IndexError(err)
     proxy_server.busy = False
-    print('free ip: {}'.format(proxy_server.ip))
+    print('free ip: {} after {} seconds...'\
+          .format(proxy_server.ip,wait_seconds))
+    time.sleep(wait_seconds)
     try:
         db_session.commit()
         res = True
+        print('free ip: {} success!'\
+              .format(proxy_server.ip))
     except Exception as e:
-        print(str(e))
+        print('free ip: {} failed: {}'\
+              .format(proxy_server,ip,str(e)))
         res = False
     db_session.close()
     return res
